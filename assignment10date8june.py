5ef6e32549b1cd67fdfe0217af98c0aa09e23494
Unknown <srishti22maurya@gmail.com>
2018-06-09 14:46:22 +0530

    assignment

--- assignment10date8june.py
+++ assignment10date8june.py
@@ -17,43 +17,35 @@ A B
 
 #solution 3
 class cop:
-    def __init__(self,name,age,work,experience,designation):
+    def __init__(self,name,age,work,experience,designation,):
         self.name=name
         self.age=age
         self.work=work
         self.experience=experience
         self.designation=designation
-    def display(self,name,age,work,experience,designation):
+    def display(self):
         print(self.name)
         print(self.age)
         print(self.work)
         print(self.experience)
         print(self.designation)
-    def update(self,name,age,work,experience,designation):
+    def update(self):
         self.name = 'bob'
         self.age = 40
         self.work = 'ceo'
         self.experience = 6
         self.designation = 'head'
-        print("updated data:")
-        print(self.name)
-        print(self.age)
-        print(self.work)
-        print(self.experience)
-        print(self.designation)
+
 class mission(cop):
     def __init(self):
-        super(mission,self).__init__()
-    def __init__(self,details):
-        self.details=details
+        super(mission,self).__init()
     def add_method_details(self,details):
-        return self.details
+        return details
     def display1(self,details):
-        print(self.details)
+        print(details)
 obj = mission('sherlock',35,'manager',5,'design')
 obj.add_method_details('secret')
 obj.display1('secret')
-obj.display('sherlock',35,'manager',5,'design')
 obj.update()
 
 
@@ -81,5 +73,6 @@ y=square(6,6)
 y.squ()
 y.area(6,6)
 '''
+
 
 
